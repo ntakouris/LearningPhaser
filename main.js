9021b57c06d0ed384ce2fd8ed2fcef2019a77a6f
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-12-08 17:29:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game_div');
 var char = null;
+var lazers = [];
 
 var main_state = {
 
     preload: function() {
-       game.load.image('char', 'assets/hello.png');  
-       game.load.audio('lazer_shoot', ['assets/lazer_shoot.wav']);
+       game.load.image('char', 'assets/robot.png');  
+       game.load.image('lazer', 'assets/lazer.png');
+       game.load.audio('lazer_shoot', 'assets/lazer_shoot.wav');
+       game.load.audio('explode', 'assets/explosion.wav');
     },
 
     create: function() { 
@@ -15,10 +18,10 @@ var main_state = {
         game.stage.backgroundColor = '#736357';
         
         char = game.add.sprite(35, 300, 'char');  
-        
         game.physics.arcade.enable(char);
         
-        velocity = -100;
+        velocity = -200;
+        lazer_velocity = 300;
         lazer_shoot_sound = game.add.audio('lazer_shoot');
         
         spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -37,7 +40,21 @@ var main_state = {
 
     update: function() {
         this.fixCharacterPositionIfOutOfScreenBounds();
-        
+        this.updateLazers();
+    },
+    
+    updateLazers: function(){
+        var newArr = [];
+      for (var i = 0; i < lazers.length; i++) {
+            if(lazers[i].x > 800){
+                lazers[i].destroy();
+            }else{
+                newArr.push(lazers[i]);
+            }
+       }
+       
+       lazers = newArr;
+       
     },
     
     moveUp: function(){
@@ -48,7 +65,7 @@ var main_state = {
         char.body.velocity.y = -velocity;
     },
     
-    fixCharacterPositionIfOutOfScreenBounds: function(){
+    fixCharacterPositionIfOutOfScreenBounds: function(){//TODO
         if(char.y < 0){
             char.y = 0;
             char.body.velocity.y = 0;
@@ -63,13 +80,30 @@ var main_state = {
     },
     
     shoot_lazer: function(){
+      var a = Math.random();
+      var offset_y = 0;
+      
+      if(a <= 0.5){
+          offset_y = 18;
+      }else{
+          offset_y = 13;
+      }
+      
+      var lazer = game.add.sprite(char.x + char.width + 3, char.y + offset_y, 'lazer'); 
+      game.physics.arcade.enable(lazer);
+      
+      lazer.body.velocity.x = lazer_velocity;
       lazer_shoot_sound.play();  
+      lazers.push(lazer);
+
     },
     
     update_score: function(){
         this.score += 10;
         this.scoreText.text = "Score: " + this.score;
-    }
+    },
+
+    
 }
 
 game.state.add('main', main_state);  
